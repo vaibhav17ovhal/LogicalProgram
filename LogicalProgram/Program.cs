45c5d12364a6a9e45ff59aca4b2f5fe618252ddd
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-01 11:39:37 +0530

    [Vaibhav] Add:Print the series

---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LogicalProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Program.");
+
+            FibonacciSeries.Series();
+        }
+    }
+}

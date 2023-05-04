69e20f79c259892ae8c95d395ac258ef98261574
vaibhav17ovhal <125484163+vaibhav17ovhal@users.noreply.github.com>
2023-05-04 20:55:00 +0530

    Update Program.cs

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -8,10 +8,6 @@ namespace LogicalProgram
         {
             Console.WriteLine("Welcome to Logical Program.");
 
-            //FibonacciSeries.Series();
-            //PerfectNumber.Number();
-            //PrimeNumber.Prime();
-            //ReverseNumber.Reverse();
             CouponNumber.Coupon(15);
         }
     }
